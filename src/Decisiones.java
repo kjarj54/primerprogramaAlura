6e72d6272cd1b2813d67be2a66333b22d194d8d1
Arauz <86898345+kjarj54@users.noreply.github.com>
2024-04-07 21:40:09 -0600

    alura class

---
+++ src/Decisiones.java
@@ -0,0 +1,13 @@
+public class Decisiones {
+    public static void main(String[] args) {
+        int fechaDeLanzamiento = 1999;
+        boolean incluidoEnElPlan = true;
+        double notaDeLaPelicula = 8.2;
+
+        if (fechaDeLanzamiento > 2022){
+            System.out.println("Pelicula mas popular");
+        }else {
+            System.out.println("Pelicula Retro que aun vale la pena ver");
+        }
+    }
+}
